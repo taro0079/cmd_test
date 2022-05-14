5fafe55f9fa96a1434e20a091f76175d2fada3dc
Taro Morita <morita0079@gmail.com>
2022-05-14 23:32:21 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use clap::Parser;
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+
+struct Args {
+    #[clap(short, long)]
+    name: String,
+    #[clap(short, long, default_value_t = 1)]
+    count: u8,
+}
+fn main() {}
